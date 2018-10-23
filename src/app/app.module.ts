ba8390082b3d5976a24a2ad837fc764a719e5192
Ricardo Mendes <ricardomlealjr@gmail.com>
2018-10-23 20:50:47 -0300

    Hello, listening e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConstrucaoComponent } from './construcao/construcao.component';
+import { HelloComponent } from './hello/hello.component';
+import { ListeningComponent } from './listening/listening.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConstrucaoComponent
+    ConstrucaoComponent,
+    HelloComponent,
+    ListeningComponent,
+    FooterComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/listening/listening.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-listening',
+    templateUrl: './listening.component.html',
+    styleUrls: ['./listening.component.css']
+})
+export class ListeningComponent implements OnInit {
+    APIKEY = 'f973e433d0f6fb9ec86954784d0240a8';
+    SHAREDSECRET = '255c4b4363d2b2729fbdeb0957da1fb0';
+    totalScrobble: number;
+    listening: boolean;
+    song: any;
+    array: Array<JSON>;
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    ngOnInit() {
+        this.http.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ricardobr001&api_key=' + this.APIKEY + '&format=json')
+        .subscribe(res => {
+            this.totalScrobble = res['recenttracks']['@attr']['total'];
+
+            if (res['recenttracks']['track'][0]['@attr']) {
+                this.listening = true;
+            } else {
+                this.listening = false;
+            }
+
+            this.song = res['recenttracks']['track'][0];
+        });
+
+        this.http.get('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=ricardobr001&api_key=' + this.APIKEY + '&format=json&limit=6')
+        .subscribe(res => {
+            console.log(res['topartists']['artist']);
+            this.array = res['topartists']['artist'];
+        });
+    }
+
+}
