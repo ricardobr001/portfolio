1605f69dcb054d1a95cbf5f3118aa3567e3055fc
Ricardo Mendes <ricardomlealjr@gmail.com>
2019-07-21 10:12:00 -0300

    Fix in the artists image

--- src/app/listening/listening.component.ts
+++ src/app/listening/listening.component.ts
@@ -9,7 +9,10 @@ import { HttpClient } from '@angular/common/http';
 export class ListeningComponent implements OnInit {
     LAST_APIKEY = 'f973e433d0f6fb9ec86954784d0240a8';
     VAGALUME_APIKEY = '660a4395f992ff67786584e238f501aa';
+    FANART_APIKEY = '6a9aed206f07b70d5e99153ac6641107';
     SHAREDSECRET = '255c4b4363d2b2729fbdeb0957da1fb0';
+    NUMBER_OF_ARTISTS = 15;
+    ARTISTS_ARRAY_SIZE = 3;
     totalScrobble: number;
     listening: boolean;
     song: any;
@@ -21,7 +24,7 @@ export class ListeningComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.http.get('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ricardobr001&api_key=' + this.LAST_APIKEY + '&format=json')
+        this.http.get(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=ricardobr001&api_key=${this.LAST_APIKEY}&format=json`)
         .subscribe(res => {
             this.totalScrobble = res['recenttracks']['@attr']['total'];
 
@@ -35,14 +38,36 @@ export class ListeningComponent implements OnInit {
             this.getLyric();
         });
 
-        this.http.get('https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=ricardobr001&api_key=' + this.LAST_APIKEY + '&format=json&limit=6')
+        this.http.get(`https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=ricardobr001&api_key=${this.LAST_APIKEY}&format=json&limit=${this.NUMBER_OF_ARTISTS}`)
         .subscribe(res => {
-            this.array = res['topartists']['artist'];
+            this.array = [];
+
+            this.randomNumbers(this.NUMBER_OF_ARTISTS).forEach(index => {
+                res['topartists']['artist'][index]['image'] = '';
+                this.array.push(res['topartists']['artist'][index]);
+            });
+
+            this.recoverArtistImage();
+            console.log(this.array);
         });
     }
 
+    randomNumbers(len: number): Array<number> {
+        const numbers = [];
+
+        while (numbers.length != this.ARTISTS_ARRAY_SIZE) {
+            let n = Math.floor(Math.random() * len);
+            while (numbers.find(x => x === n)) {
+                n = Math.floor(Math.random() * len);
+            }
+            numbers.push(n);
+        }
+
+        return numbers;
+    }
+
     getLyric() {
-        this.http.get('https://api.vagalume.com.br/search.php?apikey=' + this.VAGALUME_APIKEY + '&art=' + this.song['artist']['#text'] + '&mus=' + this.song['name'])
+        this.http.get(`https://api.vagalume.com.br/search.php?apikey=${this.VAGALUME_APIKEY}&art=${this.song['artist']['#text']}&mus=${this.song['name']}`)
         .subscribe(res => {
             if (!res['type'].localeCompare('song_notfound') || !res['type'].localeCompare('notfound')) {
                 this.lyric = '404 NOT FOUND<br>¯\\_(ツ)_/¯';
@@ -52,8 +77,31 @@ export class ListeningComponent implements OnInit {
         });
     }
 
+    recoverArtistImage() {
+        this.array.forEach((obj, index) => {
+            this.artistMusicBrainzId(obj['name'], index);
+        });
+    }
+
+    artistMusicBrainzId(artist: string, index: number) {
+        this.http.get(`https://musicbrainz.org/ws/2/artist/?query=artist:${artist}&fmt=json`)
+        .subscribe(res => {
+            this.artistFanartPicture(res['artists'][0]['id'], index);
+        });
+    }
+
+    artistFanartPicture(id: string, index: number) {
+        this.http.get(`https://webservice.fanart.tv/v3/music/${id}&?api_key=${this.FANART_APIKEY}&format=json`)
+        .subscribe(res => {
+            const randomImageIndex = Math.floor(Math.random() * res['artistbackground'].length);
+            this.array[index]['image'] = res['artistbackground'][randomImageIndex]['url'];
+        },
+        err => {
+            this.array[index]['image'] = 'assets/img/rockbandnotfound.png';
+        });
+    }
+
     reload() {
         this.ngOnInit();
     }
-
 }
